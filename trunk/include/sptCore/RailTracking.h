d4b654727cce85f3b1a4d34aca8a74a29b0e5c7c
ShaXbee <devnull@localhost>
2009-03-05 01:34:59 +0100

    [svn r8]

--- trunk/include/sptCore/RailTracking.h
+++ trunk/include/sptCore/RailTracking.h
@@ -1,24 +1,33 @@
 #ifndef SPTCORE_RAILTRACKING_H
 #define SPTCORE_RAILTRACKING_H 1
 
-#include <vector>
-
-#include <osg/Vec3>
-#include <osg/Node>
+#include <osg/Array>
 
 namespace sptCore
 {
+    
+class Path;
 
-class RailTracking: public osg::Node
+class RailTracking
 {
 
 public:
-    typedef std::vector<osg::Vec3f> Path;
-
     virtual RailTracking* getNext(RailTracking* tracking) = 0;
     virtual Path* getPath(RailTracking* tracking) = 0;
+    virtual Path* reverse(Path* path) = 0;
 
 }; // class sptCore::RailTracking
+
+struct Path
+{
+    
+    Path* reverse();
+    
+    RailTracking* _begin;
+    RailTracking* _end;
+    osg::ref_ptr<osg::Vec3Array> _points;
+    
+}; // class sptCore::Path
 
 } // namespace sptCore
 
--- trunk/src/sptCore/RailTracking.cpp
+++ trunk/src/sptCore/RailTracking.cpp
@@ -1,3 +1,21 @@
+#include <algorithm>
+
 #include "sptCore/RailTracking.h"
 
 using namespace sptCore;
+
+Path* Path::reverse()
+{
+    
+    Path* result = new Path;
+    
+    result->_begin = _end;
+    result->_end = _begin;
+    
+    result->_points = new osg::Vec3Array(_points->size());
+    
+    std::reverse_copy(_points->begin(), _points->end(), result->_points->begin());
+    
+    return result;
+    
+}; // Path::reverse
