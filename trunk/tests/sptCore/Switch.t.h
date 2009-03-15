1c0629600a08a431e18c026a1bc324b487aa468d
ShaXbee <devnull@localhost>
2009-03-15 23:40:16 +0100

    [svn r20]

--- trunk/tests/sptCore/Switch.t.h
+++ trunk/tests/sptCore/Switch.t.h
@@ -49,19 +49,31 @@ public:
     {
         
         _switch.setPosition(STRAIGHT);
-        
+
+        // _begin -> _straight        
         TS_ASSERT_EQUALS(_switch.getPath(_begin)->front, _straight);
+        // _diverted -> _begin
         TS_ASSERT_EQUALS(_switch.getPath(_diverted)->front, _begin);
+        // _straight -> _begin
+        TS_ASSERT_EQUALS(_switch.getPath(_straight)->front, _begin);
+        // _diverted != _straight        
         TS_ASSERT_DIFFERS(_switch.getPath(_diverted)->front, _switch.getPath(_straight)->front);
-                
+
+        // incorrect entry point
         TS_ASSERT_THROWS(_switch.getPath(osg::Vec3f(0.0f, 0.0f, 1.0f)), RailTracking::UnknownEntryException);
         
         _switch.setPosition(DIVERTED);        
         
+        // _begin -> _diverted
         TS_ASSERT_EQUALS(_switch.getPath(_begin)->front, _diverted);
+        // _diverted -> _begin
         TS_ASSERT_EQUALS(_switch.getPath(_diverted)->front, _begin);
+        // _straight -> _begin
+        TS_ASSERT_EQUALS(_switch.getPath(_straight)->front, _begin);
+        // _diverted != _straight
         TS_ASSERT_DIFFERS(_switch.getPath(_diverted)->front, _switch.getPath(_straight)->front);
                 
+        // incorrect entry point
         TS_ASSERT_THROWS(_switch.getPath(osg::Vec3f(0.0f, 0.0f, 1.0f)), RailTracking::UnknownEntryException);
        
     };
@@ -69,8 +81,14 @@ public:
     void testReverse()
     {
         
+        _switch.setPosition(STRAIGHT);        
+        
+        // _begin, _straight -> _straight, _begin
         TS_ASSERT_EQUALS(_switch.getPath(_begin), _switch.reverse(_switch.getPath(_straight)));
+        // _begin, _straight != _begin -> _straight
         TS_ASSERT_DIFFERS(_switch.getPath(_begin), _switch.reverse(_switch.getPath(_begin)));
+        // _diverted, _begin != _straight -> _begin
+        TS_ASSERT_DIFFERS(_switch.getPath(_diverted), _switch.reverse(_switch.getPath(_begin)));
         
     };
     
