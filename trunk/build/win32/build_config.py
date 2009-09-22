4464e927a07ff97ac7e286a24361b9e9ed76d103
ShaXbee <devnull@localhost>
2009-09-22 17:52:30 +0200

    [svn r76] Update for MSVC build

--- trunk/build/win32/build_config.py
+++ trunk/build/win32/build_config.py
@@ -1,5 +1,7 @@
 # DO NOT COMMIT CHANGES IN THIS FILE
 # each developer should edit this file to customize paths for system
 
-OPENSCENEGRAPH_PATH     = r"d:\projects\OpenSceneGraph-2.8.0"
-OPENSCENEGRAPH_DEP_PATH = r"d:\projects\OpenSceneGraph-2.8.0\3rdParty"
+OPENSCENEGRAPH_PATH     = r"D:\mandziej\workspace\OpenSceneGraph-2.8.2"
+OPENSCENEGRAPH_DEP_PATH = r"D:\mandziej\workspace\OpenSceneGraph-2.8.2\3rdParty"
+
+BOOST_PATH				= r"C:\Program Files\boost\boost_1_38"
