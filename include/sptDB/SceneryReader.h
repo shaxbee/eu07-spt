d5ada7ea5f47dff007f542442d4a2dd3bf6ba4f3
Zbyszek Mandziejewicz <shaxbee@eu07.pl>
2010-06-09 17:55:21 +0200

    Added *.swp to hgignore, begin work on SceneryReader

--- include/sptDB/SceneryReader.h
+++ include/sptDB/SceneryReader.h
@@ -7,11 +7,58 @@ namespace sptDB
 class SceneryReader
 {
 
+public:
+    SceneryReader(std::istream& stream): _input(stream) { }
+
 private:
-    loadTrack();
-    loadSwitch();
+    void readInt(int& output);
+    void readUInt(unsigned int& output);
+    void readFloat(float& output);
+    void readChars(std::string& output, size_t count);
+    void readString(std::string& output);
+    template <typename T>
+    void readVector(std::vector<typename T>& output);
+
+    size_t readChunk(const std::string& type);
+
+    void readHeader();
+    void readSectorOffsets();
+    void readSectorData(unsigned int id);
+
+    struct Header
+    {
+        unsigned int dataSize;
+        unsigned int version;
+    };
+
+    std::istream& _input;
+    size_t _chunkBytesLeft;
+
+    typedef std::vector<unsigned int> SectorOffsets;
+    SectorOffsets _sectorOffsets;
 
 }; // class sptDB::SceneryReader
+
+template <typename T>
+SceneryReader::readVector(<std::vector<typename T>& output)
+{
+    unsigned int size;
+    readUInt(size);
+
+    const unsigned int elementSize = sizeof(T);
+
+    assert(elementSize * size < _chunkBytesLeft);
+    assert(output.empty());
+
+    output.reserve(size);
+
+    while(size--)
+    {
+        T element;
+        _input.read(reinterpret_cast<char*>(&element), elementSize);
+        output.push_back(element);
+    };
+};
 
 }; // namespace sptDB
 
