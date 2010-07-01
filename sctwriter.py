82d8309acaea60a4f883e9765dbe7a27a5de85fc
ShaXbee <devnull@localhost>
2010-07-01 22:47:28 +0200

    Sector writer optimizations

--- sctwriter.py
+++ sctwriter.py
@@ -1,12 +1,24 @@
-import struct
+from struct import Struct
 
 class BinaryWriter(object):
-    uIntFormat = struct.Struct("I")
-    vec3fFormat = struct.Struct("fff")
-    vec3dFormat = struct.Struct("ddd")
+    uIntFormat = Struct("I")
+    vec3fFormat = Struct("fff")
+    vec3dFormat = Struct("ddd")
 
     def __init__(self, ifile):
         self.__input = ifile
+
+    def writeArray(self, fmt, data, count):
+        buf = bytearray(fmt.size * count)
+        offset = 0
+        for values in data:
+            fmt.pack_into(buf, offset, *values)
+            offset += fmt.size
+        self.__input.write(name)
+
+    def writeVarArray(self, data):
+        for fmt, values in data:
+            self.__input.write(fmt.pack(*values))
 
     def writeChunk(self, name, data):
         self.__input.write(name)
@@ -18,33 +30,47 @@ class BinaryWriter(object):
         self.__input.write(value)
 
     def writeUInt(self, value):
-        self.__input.write(BinaryWriter.uIntFormat.pack(value))
+        self.writeFmt(uIntFormat, value)
 
     def writeVec3f(self, value):
-        self.__input.write(BinaryWriter.vec3fFormat.pack(value.x, value.y, value.z))
+        self.writeFmt(vec3fFormat, value.as_tuple())
 
     def writeVec3d(self, value):
-        self.__input.write(BinaryWriter.vec3dFormat.pack(value.x, value.y, value.z))
+        self.writeFmt(vec3dFormat, value.as_tuple())
 
-class SectorWriter(object):
-    def __init__(self, ifile, data):
-        self.__writer = BinaryWriter(ifile)
+    def writeFmt(self, fmt, values):
+        self.__input.writer(fmt.pack(*values))
 
-    def __writePath(self, path):
-        self.writeUInt(path.kind)
-        if path.kind == 0:
-            self.writeVec3f(path.begin)
-            self.writeVec3f(path.end)
-        elif path.kind == 1:
-            self.writeVec3f(path.begin)
-            self.writeVec3f(path.beginCP)
-            self.writeVec3f(path.end)
-            self.writeVec3f(path.endCP)
+__trackFormats = [
+    Struct("B 3f3f"), # straight
+    Struct("B 3f3f3f3f") # bezier
+]
+
+__switchFormats = [
+    Struct("B 3f3f 3f3f"), # straight, straight
+    Struct("B 3f3f 3f3f3f3f"), # straight, bezier
+    Struct("B 3f3f3f3f 3f3f"), # bezier, straight
+    Struct("B 3f3f3f3f 3f3f3f3f") # bezier, bezier
+]
+
+def __tracksGen(tracks, offset):
+    for track in tracks:
+        yield track.path.points(offset)
+
+def __switchGen(switches, offset):
+    for switch in switches:
+        fmt = __switchFormats[switch.straight.kind + switch.diverted.kind * 2]
+        yield (fmt, switch.straight.points(offset) + switch.diverted.points(offset))
+
+class SectorWriter(BinaryWriter):
+    def __init__(self, ifile):
+        self.__init__(ifile)
 
     def __writeTrackList(self):
-        self.writeUInt(len(self.data.tracks))
-        for track in self.data.tracks:
-            self.__writePath(track.path)
+        for kind in range(2):
+            size = len(self.data.tracks[PathKind.STRAIGHT])
+            self.writeUInt(size)
+            self.writeArray(__trackFormats[kind], __tracksGen(self.data.tracks[kind], self.__offset), size)
 
     def __writeSwitchList(self):
         self.writeUInt(len(self.data.switches))
