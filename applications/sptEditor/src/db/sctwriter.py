9fda5dff8ddb16a29d344a17eba90f29ef71a86a
ShaXbee <devnull@localhost>
2010-07-26 00:06:21 +0200

    Connections writing work.

--- applications/sptEditor/src/db/sctwriter.py
+++ applications/sptEditor/src/db/sctwriter.py
@@ -83,6 +83,11 @@ _switchFormats = [
     Struct("<B B 3f3f3f3f B 3f3f3f3f") # position, bezier, bezier
 ]
 
+_internalConnectionFormat = Struct("<3f3f3f I I") # position, from index, to index
+_externalConnectionFormat = Struct("<3f3f3f I") # position, index
+
+SECTOR_SIZE = 1000
+
 class PathKind(object):
     STRAIGHT = 0
     BEZIER = 1
@@ -188,21 +193,36 @@ class SectorWriter(BinaryWriter):
         return index
 
     def __buildTrackingIndex(self):
-        self.__index = self.__buildTrackingIndexImpl(self.__tracks)
-        self.__index.extend(self.__buildTrackingIndexImpl(self.__switches, len(self.__tracks)))
+        self.__index = self.__buildTrackingIndexImpl(self.__tracks[PathKind.STRAIGHT])
 
-    def __addInternalConnection(self, position, from, to):
-        if position not in self.__connections:
-            self.__connections[position] = (self.__index[from], self.__index[to])
+        offset = len(self.__tracks[PathKind.STRAIGHT])
+        self.__index = self.__buildTrackingIndexImpl(self.__tracks[PathKind.BEZIER], offset)
 
-    def __collectConnections(self):
-        self.__connections = dict()
+        offset += len(self.__tracks[PathKind.BEZIER])
+        self.__index.update(self.__buildTrackingIndexImpl(self.__switches, offset))
 
-        for track in self.__tracks:
+    def __addConnection(self, position, left, right):
+        if position.x < 0 or position.x > SECTOR_SIZE or position.y < 0 or position.y > SECTOR_SIZE:
+            if position not in self.__externalConnections:
+                self.__externalConnections[position] = self.__index[left]
+        else:
+            if position not in self.__internalConnections:
+                self.__internalConnections[position] = (self.__index[left], self.__index[right])
+
+    def __collectTracksConnections(self, source):
+        self.__buildTrackingIndex()
+        for track in source:
             if track.n1:
                 self.__addConnection(track.p1, track, track.n1)
             if track.n2:
                 self.__addConnection(track.p2, track, track.n2)
+
+    def __collectConnections(self):
+        self.__internalConnections = dict()
+        self.__externalConnections = dict()
+
+        self.__collectTracksConnections(self.__tracks[PathKind.STRAIGHT])
+        self.__collectTracksConnections(self.__tracks[PathKind.BEZIER])
 
         for switch in self.__switches:
             if switch.n1:
@@ -212,32 +232,49 @@ class SectorWriter(BinaryWriter):
             if switch.n3:
                 self.__addConnection(switch.p3, switch, switch.n3)
 
+    def __writeConnections(self):
+        self.beginChunk("CNLS")
+
+        self.__collectConnections()
+
+        self.writeUInt(len(self.__internalConnections))
+        self.writeArray(
+            _internalConnectionFormat, 
+            ((position, ) + indexes for position, indexes in self.__internalConnections.iteritems()),
+            len(self.__internalConnections))
+
+        self.writeUInt(len(self.__externalConnections))
+        self.writeArray(_externalConnectionFormat, self.__externalConnections.iteritems(), len(self.__externalConnections))
+
+        self.endChunk("CNLS")
+
     def writeToFile(self):
         self.beginChunk("SECT")
         self.__writeTrackList()
         self.__writeSwitchList()
         self.__writeTrackNames()
         self.__writeSwitchNames()
+        self.__writeConnections()
         self.endChunk("SECT")
 
     def __getPath(self, p1, v1, p2, v2):
-        if not v1 and not v2:
+        if v1 == p1 and v2 == p2:
             return StraightPath(p1, p2)
         
         return BezierPath(p1, v1, p2, v2)
 
     def addTrack(self, track):
-        track.p1 -= offset
-        track.p2 -= offset
+        track.p1 -= self.__offset
+        track.p2 -= self.__offset
 
         track.path = self.__getPath(track.p1, track.v1, track.p2, track.v2)
 
         self.__tracks[track.path.kind].append(track)
 
     def addSwitch(self, switch):
-        switch.p1 -= offset
-        switch.p2 -= offset
-        switch.p3 -= offset
+        switch.p1 -= self.__offset
+        switch.p2 -= self.__offset
+        switch.p3 -= self.__offset
 
         switch.straight = self.__getPath(switch.p1, switch.v1, switch.p2, switch.v2)
         switch.diverted = self.__getPath(switch.p1, switch.v1, switch.p3, switch.v3)
@@ -246,22 +283,53 @@ class SectorWriter(BinaryWriter):
 
 if __name__ == "__main__":
     class Track(object):
-        def __init__(self, path, name = None):
-            self.path = path
+        def __init__(self, p1, v1, p2, v2, name = None):
+            self.p1 = p1
+            self.v1 = v1
+            self.p2 = p2
+            self.v2 = v2
+
+            self.n1 = None
+            self.n2 = None
+
             self.name = name
 
+        def connect(self, point, tracking):
+            if point == self.p1:
+                self.n2 = tracking
+            elif point == self.p2:
+                self.n1 = tracking
+            else:
+                raise RuntimeError("Unknown point")
+
     class Switch(object):
-        def __init__(self, straight, diverted, position = "STRAIGHT", name = None):
-            self.straight = straight
-            self.diverted = diverted
+        def __init__(self, p1, v1, p2, v2, p3, v3, position = "STRAIGHT", name = None):
+            self.p1 = p1
+            self.v1 = v1
+            self.p2 = p2
+            self.v2 = v2
+            self.p3 = p3
+            self.v3 = v3
+
+            self.n1 = None
+            self.n2 = None
+            self.n3 = None
+
             self.position = position
             self.name = name
 
     output = file("test.sct", "w+")
     writer = SectorWriter(output, Vec3())
-    writer.addTrack(Track(StraightPath(Vec3(100, 100, 0), Vec3(200, 100, 0)), "start"))
-    writer.addTrack(Track(BezierPath(Vec3(), Vec3(), Vec3(), Vec3(100, 100, 0))))
-    writer.addSwitch(Switch(StraightPath(Vec3(0, 0, 0), Vec3(100, 0, 0)), StraightPath(Vec3(0, 0, 0), Vec3(0, 100, 0))))
+
+    zero = Vec3(0, 0, 0)
+
+    t1 = Track(Vec3(100, 100, 0), Vec3(100, 100, 0), Vec3(200, 100, 0), Vec3(200, 100, 0), "start")
+    t2 = Track(zero, zero, Vec3(100, 100, 0), Vec3(0, 0, 100))
+    t1.connect(Vec3(100, 100, 0), t2)
+
+    writer.addTrack(t1)
+    writer.addTrack(t2)
+    writer.addSwitch(Switch(zero, zero, Vec3(100, 0, 0), Vec3(100, 0, 0), Vec3(0, 100, 0), Vec3(0, 100, 0)))
     writer.writeToFile()
 
     output.close()
